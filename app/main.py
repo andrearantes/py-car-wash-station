8f4f6f67759392df7c9afd0ac7228e8bc76822c6
Andre Marcio <andre.marcio.ti@gmail.com>
2026-03-12 16:56:58 -0300

    verify task

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float, count_of_ratings: int) -> None:
+    def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float, count_of_ratings: int) -> float:
         """Representa uma estação de lavagem de carros com seus atributos principais"""
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
